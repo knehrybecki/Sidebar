668b19c8113b28fc6ced94aaf0ff87ce9b94bae2
Kamil Nehrybecki <knehrybecki@gmail.com>
2022-02-10 23:57:57 +0100

    corrected

--- script.js
+++ script.js
@@ -1,25 +1,14 @@
-const openMenu = document.querySelector('.fas.fa-bars');
-const closeMenu = document.querySelector('.fa-solid.fa-xmark');
-const menu = document.querySelector('.card__sidebar');
-const nav = document.querySelector('.navigation');
-const menuItem = document.querySelector('.sidebar__menu');
-const logoItem = document.querySelector('.navigation__logo');
+const openMenu = document.querySelector('.fas.fa-bars')
+const closeMenu = document.querySelector('.fa-solid.fa-xmark')
+const menu = document.querySelector('.card__navigation')
 
-openMenu.addEventListener('click', function()
-{
+openMenu.addEventListener("click", () => {
         menu.classList.add('showMenu')
-        nav.classList.add('showMenu')
-        menuItem.classList.add('showMenu')
-        logoItem.classList.add('showMenu')
         closeMenu.style.display = 'block'
         openMenu.style.display = 'none'
 })
-closeMenu.addEventListener('click', function()
-{       
+closeMenu.addEventListener("click", () => {       
         menu.classList.remove('showMenu')
-        nav.classList.remove('showMenu')
-        menuItem.classList.remove('showMenu')
-        logoItem.classList.remove('showMenu')
         openMenu.style.display = 'block'
         closeMenu.style.display = 'none'
 })
